276f3f1be47254997a3d36f81411a400d29ed9be
Diarmuid O Connor <doconnor@wit.ie>
2021-09-18 12:45:29 +0100

    Initial structure.

---
+++ src/productCatalogue.js
@@ -0,0 +1,24 @@
+class Catalogue {
+  constructor(title) {
+    this.title = title;
+    this.products = [];
+  }
+
+  findProductById(id) {
+    const match = this.products.find(
+      function (product) {
+        return id === product.id;
+      })
+    return match;
+  }
+
+  addProduct(product) {
+    if (!this.findProductById(product.id)) {
+      this.products.push(product);
+      return true;
+    }
+    return false;
+  }
+
+}
+module.exports = Catalogue;
